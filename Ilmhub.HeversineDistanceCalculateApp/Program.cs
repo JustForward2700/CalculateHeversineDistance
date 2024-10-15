ff57d911549907738fedfd7ce796a29d5b13bed7
JustForward2700 <feruzbekraximboyev2700@gmail.com>
2024-10-15 20:37:13 +0500

    My Two commit

--- Ilmhub.HeversineDistanceCalculateApp/Program.cs
+++ Ilmhub.HeversineDistanceCalculateApp/Program.cs
@@ -1,17 +1,11 @@
 ﻿using Ilmhub.HeversineDistanceCalculateApp;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        System.Console.WriteLine("1-pointni koordinatalarin verugul bilan ajratib kiriting: ");
-        Coordinate pointOne = new Coordinate(Console.ReadLine());
-        System.Console.WriteLine("2-pointni koordinatalarin verugul bilan ajratib kiriting: ");
-        Coordinate pointTwo = new Coordinate(Console.ReadLine());
-        double distance = pointOne.GetHaversineDistance(pointTwo);
-        Console.WriteLine($"Natija: 1 va 2 pointlar orasidagi masofa {distance} Metr!");
-    }
-} 
+System.Console.WriteLine("1-pointni koordinatalarin verugul bilan ajratib kiriting: ");
+Coordinate pointOne = new Coordinate(Console.ReadLine());
+System.Console.WriteLine("2-pointni koordinatalarin verugul bilan ajratib kiriting: ");
+Coordinate pointTwo = new Coordinate(Console.ReadLine());
+double distance = pointOne.GetHaversineDistance(pointTwo);
+Console.WriteLine($"Natija: 1 va 2 pointlar orasidagi masofa {distance} Metr!");
 
 
 
